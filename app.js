4ff7451e336609651bbe6e6cf888d86505d80c08
Manish Jethani <manish.jethani@gmail.com>
2013-10-24 09:33:47 +0530

    Compress.

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.set('views', themeDirectory);
 app.set('view engine', 'ejs');
 //app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.compress());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
